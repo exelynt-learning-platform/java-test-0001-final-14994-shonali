f9a7fbd33c4c0fdeaf804f7ede62d7ca663fa8fa
ShonaliNikam <shonalinikam14@gmail.com>
2026-03-14 16:53:07 +0530

    Added diamond problem

---
+++ Diamond.java
@@ -0,0 +1,39 @@
+public class Diamond {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the top half
+
+        // Top Half
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars and hollow spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Bottom Half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars and hollow spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
